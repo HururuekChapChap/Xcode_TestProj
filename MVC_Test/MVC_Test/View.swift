679c42c4a03e1b201a578ba5003344cac38b0972
HururuekChapChap <57618897+HururuekChapChap@users.noreply.github.com>
2020-10-02 23:21:54 +0900

    MVC_Test

---
+++ MVC_Test/MVC_Test/View.swift
@@ -0,0 +1,44 @@
+//
+//  View.swift
+//  MVC_Test
+//
+//  Created by yoon tae soo on 2020/10/02.
+//
+
+import UIKit
+
+class View: UIView {
+    
+    var label : UILabel = {
+        
+        let label = UILabel()
+        label.textColor = .white
+        label.textAlignment = .center
+        return label
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        backgroundColor = .red
+        addSubview(label)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func changeLabel(_ text : String){
+        print(text)
+        label.text = text
+//        setNeedsLayout()
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        print("layoutSubviews")
+        label.sizeToFit()
+        label.center = CGPoint(x: self.bounds.width / 2, y: self.bounds.height / 2)
+        
+    }
+
+}
