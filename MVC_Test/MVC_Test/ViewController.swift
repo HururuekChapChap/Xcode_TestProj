fd58a62e628e73b345ccd913bc3cb2b1ecf86a3d
HururuekChapChap <57618897+HururuekChapChap@users.noreply.github.com>
2020-10-06 23:38:07 +0900

    MVC_Test

--- MVC_Test/MVC_Test/ViewController.swift
+++ MVC_Test/MVC_Test/ViewController.swift
@@ -11,6 +11,7 @@ class ViewController: UIViewController {
 
     var myView : View?
     var tistroy : Info?
+    var flag : Bool = true
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -28,10 +29,34 @@ class ViewController: UIViewController {
     override func viewDidLayoutSubviews() {
         super.viewDidLayoutSubviews()
         print("viewDidLayoutSubView")
+        
+        if flag {
+        
         myView = View(frame: CGRect(x: 0, y: 0, width: 200, height: 200))
         myView!.center = view.center
         myView!.changeLabel(tistroy!.makeText())
-        view.addSubview(myView!)
+        view.addSubview(myView!) //subview를 해줘야 layoutsubView가 발동한다.
+//            myView!.removeFromSuperview()
+            //removeFromSuperView를 하면 layoutSubView 호출이 되지 않는다.
+            //그래서 추측으로는 코드가 다 되고 나서 update Cycle이라는 것이 있는데, 그 순간에 되는거 같다.
+            //즉 viewDidLayoutSubView 와 ViewDidAppear 사이에 update Cycle이라는 것이 있는 것 같다.
+        
+            flag = false
+        }
+        else{
+            myView!.removeFromSuperview()
+        }
+       
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        print("viewWillAppear")
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        print("viewDidAppear")
     }
     
     @IBAction func changeBTN(_ sender: UIButton) {
