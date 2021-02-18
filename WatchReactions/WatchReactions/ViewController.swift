f7e6229521b31c5964daa9b0363806818503d2cd
HururuekChapChap <57618897+HururuekChapChap@users.noreply.github.com>
2021-02-18 23:29:40 +0900

    watchos tutorial

---
+++ WatchReactions/WatchReactions/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WatchReactions
+//
+//  Created by yoon tae soo on 2021/02/18.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
